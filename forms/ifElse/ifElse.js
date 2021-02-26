802983f712336666a4b33acd8f45975eafd89258
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-02-26 15:36:41 -0600

    prompts and array defined
    prompts and array defined

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,3 @@
+let messages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "where your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+let state = prompt("What state are you from (in two letters)?")
+let currentTemp = prompt("What is the current temperature (fahrenheit)?")
