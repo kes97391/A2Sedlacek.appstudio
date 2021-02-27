99634a9cc4584e59bd77f924b52614bd983a73da
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-02-27 11:57:59 -0600

    Comments added
    I finished the program with comments as finishing touches

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,7 +1,13 @@
+/* I am defining an array which includes all of the possible resulting messages from the program as items*/
 let messages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+
+/*I am defining two prompts: one of which asks for the user's state 
+while the other one asks for the user's current temperature in said state*/
 let state = prompt("What state are you from (in two letters)?")
 let currentTemp = prompt("What is the current temperature (Write only the number in degrees fahrenheit)?")
 
+/*I constructed an ifelse statement that takes the inputs from the user 
+and produces a specific array item displayed as a message in an alert popup*/
 if (state == 'NE' && currentTemp <= 32)
   alert(`${messages[0]}`)
 else if (state == 'NE' && currentTemp >= 32 && currentTemp <=50)
