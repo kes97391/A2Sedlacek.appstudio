4ef46e872a5b502f48b5db106d4f81e904be80aa
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-02-27 11:37:37 -0600

    Update ifElse.js
    I first thought I needed to include every state, I have since changed my code to fit the instructions

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,3 +1,14 @@
-let messages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "where your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+let messages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
 let state = prompt("What state are you from (in two letters)?")
-let currentTemp = prompt("What is the current temperature (fahrenheit)?")
+let currentTemp = prompt("What is the current temperature (Write only the number in degrees fahrenheit)?")
+
+if (state == 'NE' && currentTemp <= 32)
+  alert(`${messages[0]}`)
+else if (state == 'NE' && currentTemp >= 32 && currentTemp <=50)
+  alert(`${messages[1]}`)
+else if (state == 'FL' && currentTemp >= 32 && currentTemp <=50)
+  alert(`${messages[2]}`)
+else if (state == 'FL' && currentTemp >= 50 && currentTemp <=70)
+  alert(`${messages[3]}`)
+
+  
