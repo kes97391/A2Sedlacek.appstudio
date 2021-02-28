6a85179d0c704ca1e42e0b4b904ded50b041f2ec
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-02-28 12:45:33 -0600

    idents and comments
    I added more idents to help with reading and commented out the variables on the switch form in order to prevent an error

--- forms/Switch/Switch.js
+++ forms/Switch/Switch.js
@@ -1,3 +1,23 @@
+/* Here is a switch that references the array and two prompts from the previous form. 
+It sifts through the message options and uploads a result to the console based on the 
+user's input. I have commented out variables to prevent an error from them already being 
+defined on the previous form */
+
+/* I am defining an array which includes all of the possible resulting messages from the program as items*/
+/*let messages = [
+      "wear a warm coat, hat, scarf and gloves.", 
+      "wear a warm coat but you won't need a hat, scarf or gloves.", 
+      "wear your warmest coat, a warm hat, a scarf, and warm gloves.", 
+      "wear a warm coat, hat and gloves. Maybe a scarf too.", 
+      "Invalid Input!"]*/
+
+/*I am defining two prompts: one of which asks for the user's state 
+while the other one asks for the user's current temperature in said state*/
+/*let state = 
+      prompt("What state are you from (in two letters)?")*/
+/*let currentTemp = 
+      prompt("What is the current temperature (Write only the number in degrees fahrenheit)?")*/
+
 switch(true) {
 case state == 'NE' && currentTemp <= 32:
   console.log(`${messages[0]}`)
